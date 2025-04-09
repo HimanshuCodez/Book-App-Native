5080be20d8ec9beaa795dd8ec8850753201caacd
Himanshu gaur <himanshugaur055@gmail.com>
2025-04-09 21:43:38 +0530

    succesfull  built login screen (image)

--- app/(auth)/index.jsx
+++ app/(auth)/index.jsx
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { View, Text } from "react-native";
+import { Image } from "expo-image";
+import React from "react";
+import styles from "../../assets/styles/login.styles";
+import { useState } from "react";
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = () => {};
   return (
-    <View>
-      <Text>Login Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.topIllustration}>
+        <Image source={require("../../assets/images/g-book.png")}
+        style={styles.illustrationImage}
+        resizeMode="contain"
+        />
+      </View>
     </View>
-  )
+  );
 }
--- components/SafeScreen.jsx
+++ components/SafeScreen.jsx
@@ -5,7 +5,11 @@ import COLORS from "../constants/colors";
 
 export default function SafeScreen({ children }) {
   const insets = useSafeAreaInsets();
-  return <View style={[styles.container, {paddingTop:insets.top}]}>{children}</View>;
+  return (
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      {children}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
